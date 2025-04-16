588342dc9fd0a87e5bdf9f6a0126cde0df5bf7a9
VonRiddarn <vonriddarn@gmail.com>
2025-04-16 10:47:13 +0200

    refactor: converted notification interfaces from models to types for better organization

--- src/features/notification-center/types.ts
+++ src/features/notification-center/types.ts
@@ -1,9 +1,29 @@
 // TODO: Implement all this shit before my zen is lost and I forget wtf I'm doing.
 
-import { BannerNotification, ToastNotification } from "./models";
-
 export type NotificationVariant = "info" | "success" | "warning" | "error";
 
 export type NotificationType = "toast" | "banner";
 
 export type NotificationItem = ToastNotification | BannerNotification;
+
+interface RootNotification {
+	id: string; // Used for rendering, internal deleteion, keys and stuff like that
+	tag?: string; // Logical identifier — used for grouping, deduplication, dismissing, etc...
+	type: NotificationType;
+	variant: NotificationVariant;
+	message: string;
+}
+
+export type ToastNotification = RootNotification & {
+	type: "toast";
+	duration: number;
+};
+
+export type BannerNotification = RootNotification & {
+	type: "banner";
+	priority: number;
+	action?: {
+		label: string;
+		onClick: () => void;
+	};
+};
