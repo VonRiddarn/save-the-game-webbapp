62d6f1fb696cf374cbcd8f291c0c44529385e392
VonRiddarn <vonriddarn@gmail.com>
2025-04-16 02:27:32 +0200

    feat: implement ToastContainer component for displaying notifications - WORK IN PROGRESS

--- src/features/notification-center/components/Toast/ToastContainer.tsx
+++ src/features/notification-center/components/Toast/ToastContainer.tsx
@@ -1,5 +1,37 @@
+// TODO: Scope this witht a CSS module later
+
+import { v4 as uuidv4 } from "uuid";
+import { useNotifications } from "../../NotificationsContext";
+
 const ToastContainer = () => {
-	return <div>ToastContainer</div>;
+	const notificationCenter = useNotifications();
+	const toasts = notificationCenter.notifications.filter((n) => n.type === "toast");
+
+	return (
+		<div className="toast-container-no-scope">
+			<button
+				onClick={() => {
+					notificationCenter.dispatch({
+						type: "PUSH",
+						payload: {
+							id: uuidv4(),
+							message: "New toast message",
+							type: "toast",
+							variant: "info",
+							duration: 5000,
+						},
+					});
+				}}
+			>
+				Make a toast
+			</button>
+			{toasts.map((t) => (
+				<p key={t.id}>
+					{t.id.substring(0, 5)}_____{t.message}
+				</p>
+			))}
+		</div>
+	);
 };
 
 export default ToastContainer;
