91ae9775b1d2c9d560b526bf9dfc817695d94cb6
VonRiddarn <vonriddarn@gmail.com>
2025-04-17 22:48:24 +0200

    docs: add comment regarding potential "playsound" feature in default notification settings

--- src/features/notification-center/state/notification-settings.default.ts
+++ src/features/notification-center/state/notification-settings.default.ts
@@ -3,4 +3,5 @@ import { NotificationSettings } from "../types/notification-settings.types";
 export const defaultSettings: NotificationSettings = {
 	autoDismiss: true,
 	dismissAfterMs: 3000,
+	// maybe "playsound" ?
 };
