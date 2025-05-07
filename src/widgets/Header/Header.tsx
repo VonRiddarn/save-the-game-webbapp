dff6b2ec8fb0ecdc9a3d0fe913d4f3ad09a46a16
VonRiddarn <vonriddarn@gmail.com>
2025-05-07 11:56:54 +0200

    feat: implement SearchBar component and update Header to use HamburgerButton

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,13 +1,15 @@
-import HamburgerToggleButton from "@/features/HamburgerMenu/components/HamburgerToggleButton/HamburgerToggleButton";
 import styles from "./Header.module.scss";
-import HamburgerNavigationMenu from "@/features/HamburgerMenu/components/HamburgerNavigationMenu/HamburgerNavigationMenu";
 import Image from "next/image";
+import HamburgerNavigationMenu from "@/features/HamburgerMenu/components/HamburgerNavigationMenu/HamburgerNavigationMenu";
+import SearchBar from "@/features/Search/components/SearchBar/SearchBar";
+import HamburgerButton from "@/features/HamburgerMenu/components/HamburgerToggleButton/HamburgerToggleButton";
 
 const Header = () => {
 	return (
 		<header className={styles["header"]}>
 			<Image src="/images/icons/icon.svg" alt="STG" width={64} height={45.5} />
-			<HamburgerToggleButton />
+			<SearchBar />
+			<HamburgerButton />
 			<HamburgerNavigationMenu />
 		</header>
 	);
