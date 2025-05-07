20d66dd82bf3a0d3de226ec897d84cfd8a61726b
VonRiddarn <vonriddarn@gmail.com>
2025-05-07 10:46:25 +0200

    feat: add logo image to header component

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,10 +1,12 @@
 import HamburgerToggleButton from "@/features/HamburgerMenu/components/HamburgerToggleButton/HamburgerToggleButton";
 import styles from "./Header.module.scss";
 import HamburgerNavigationMenu from "@/features/HamburgerMenu/components/HamburgerNavigationMenu/HamburgerNavigationMenu";
+import Image from "next/image";
 
 const Header = () => {
 	return (
 		<header className={styles["header"]}>
+			<Image src="/images/icons/icon.svg" alt="STG" width={64} height={45.5} />
 			<HamburgerToggleButton />
 			<HamburgerNavigationMenu />
 		</header>
