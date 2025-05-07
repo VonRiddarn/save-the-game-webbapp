3165bdc4425f500fa062f948bf7c13223fd763ce
VonRiddarn <vonriddarn@gmail.com>
2025-05-07 16:52:20 +0200

    feat: update search bar to fetch and display entities based on user input

--- src/features/Search/widgets/SearchBar/SearchBar.tsx
+++ src/features/Search/widgets/SearchBar/SearchBar.tsx
@@ -3,31 +3,53 @@
 import { useIsMobile } from "@/hooks/useIsMobile";
 import DesktopSearchBar from "../../components/DesktopSearchbar/DesktopSearchbar";
 import { useEffect, useRef, useState } from "react";
-
-// TODO: !! NOTE
-// Note:
-// This is where the API calls and data will live.
-// The child components like desktop and mobile will just be vessels to visualize the data.
-// This means that we can dynamically change the size of the viewport and keep the bar active when switching.
+import { IGDBEntityExplicit } from "@/services/igdb/types";
+import { useIGDB } from "@/hooks/useIGDB";
 
 const Searchbar = () => {
 	const [currentInput, setCurrentInput] = useState("");
+	const [fetchedEntities, setFetchedEntities] = useState<IGDBEntityExplicit[]>([]);
 	const timerRef = useRef<NodeJS.Timeout | null>(null);
 	const isMobile = useIsMobile();
+
+	// 3 different searches for easier semantics
+	const games = useIGDB();
+	const companies = useIGDB();
+	const characters = useIGDB();
 
 	// Debounce search
 	const handleChange = (newValue: string) => {
 		setCurrentInput(newValue);
 
-		// We need to use newValue because  setCurrentInput is not updated until next render
-		if (newValue.length < 2) return;
-
 		if (timerRef.current) {
 			clearTimeout(timerRef.current);
 		}
 
-		timerRef.current = setTimeout(() => {
-			console.log("Timer expired, perform search for:", newValue);
+		// We need to use newValue because  setCurrentInput is not updated until next render
+		if (newValue.length < 2) {
+			setFetchedEntities([]);
+			return;
+		}
+
+		timerRef.current = setTimeout(async () => {
+			setFetchedEntities([]);
+
+			// This is fugly.
+			// Normally I'd make one generic search to the /search api and typecheck the returns
+			// but time restrictions makes this the "best" solution for now.
+			const [gameData, companyData, characterData] = await Promise.all([
+				games.query("games", `fields *; search "${newValue}"; limit 5;`),
+				companies.query("companies", `fields *; search "${newValue}"; limit 5;`),
+				characters.query("characters", `fields *; search "${newValue}"; limit 5;`),
+			]);
+
+			const results = [
+				...(Array.isArray(gameData) ? gameData : []),
+				...(Array.isArray(companyData) ? companyData : []),
+				...(Array.isArray(characterData) ? characterData : []),
+			] as IGDBEntityExplicit[];
+
+			setFetchedEntities(results);
 			timerRef.current = null;
 		}, 500);
 	};
@@ -44,7 +66,7 @@ const Searchbar = () => {
 	return isMobile ? (
 		<h3>MOBILE DETECTED: {currentInput}</h3>
 	) : (
-		<DesktopSearchBar currentInput={currentInput} onChange={handleChange} entities={[]} />
+		<DesktopSearchBar currentInput={currentInput} onChange={handleChange} entities={fetchedEntities} />
 	);
 };
 
