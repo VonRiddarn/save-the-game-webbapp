50d604e2dfbd4db485fe39a94acf7e30f88edd50
VonRiddarn <vonriddarn@gmail.com>
2025-05-08 09:39:35 +0200

    feat: collect dead links and fallback by making a search + dispatching a notification

--- src/app/games/[slug]/GamePage.tsx
+++ src/app/games/[slug]/GamePage.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { useNotifications } from "@/features/notification-center";
 import { useIGDB } from "@/hooks/useIGDB";
 import { getCachedEntity, setCachedEntity } from "@/services/igdb/cache";
 import { Game } from "@/services/igdb/types";
-import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
 
 type GamePageProps = {
 	slug: string;
@@ -12,7 +14,11 @@ type GamePageProps = {
 const GamePage = ({ slug }: GamePageProps) => {
 	const { query, loading, error } = useIGDB<Game[]>();
 	const [game, setGame] = useState<Game | null>(null);
+	const hasFiredRef = useRef(false);
 	const ENDPOINT = "games";
+
+	const notifications = useNotifications();
+	const router = useRouter();
 
 	useEffect(() => {
 		// Note: The cache uses the session storage and will only clear after the window has closed.
@@ -20,16 +26,33 @@ const GamePage = ({ slug }: GamePageProps) => {
 		const cached = getCachedEntity(ENDPOINT, slug);
 		if (cached) {
 			setGame((cached as Game) ?? null);
+			hasFiredRef.current = true;
 			return;
 		}
 
 		query(ENDPOINT, `fields *; where slug = "${slug}";`).then((data) => {
+			if (hasFiredRef.current) return;
 			if (data && data.length > 0) {
 				setGame(data[0]);
 				setCachedEntity(ENDPOINT, slug, data[0]);
+			} else {
+				notifications.dispatch({
+					type: "PUSH",
+					payload: {
+						message: `Couldn't find "${ENDPOINT}/${slug}". Redirecting...`,
+						type: "toast",
+						persist: true,
+						timestamp: new Date().getTime(),
+						id: Math.random().toString(36).substr(2, 9),
+						severity: "error",
+					},
+				});
+				router.push("/search");
 			}
+
+			hasFiredRef.current = true;
 		});
-	}, [query, slug]);
+	}, [notifications, query, router, slug]);
 
 	if (loading && !game) return <p>Loading...</p>;
 	if (error) return <p>Error: {error}</p>;
