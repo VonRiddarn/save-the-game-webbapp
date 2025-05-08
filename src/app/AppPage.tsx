7dc75088a18749214bbddfd4c9caf97d47c96023
VonRiddarn <vonriddarn@gmail.com>
2025-05-08 11:01:19 +0200

    feat: display visited entities using getCachedEntitiesAsArray

--- src/app/AppPage.tsx
+++ src/app/AppPage.tsx
@@ -1,11 +1,24 @@
 "use client";
 
 import DGameList from "@/debug/DGameList";
+import { getCachedEntitiesAsArray } from "@/services/igdb/cache";
 
 const AppPage = () => {
+	const entities = getCachedEntitiesAsArray();
+
 	return (
 		<main>
 			<DGameList />
+			<div>
+				<h2>visited</h2>
+				{entities.map((er) => {
+					return (
+						<p key={er.entity.id}>
+							{er.endpoint} :: {er.entity.name}
+						</p>
+					);
+				})}
+			</div>
 		</main>
 	);
 };
