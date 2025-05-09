4e5e0a8bc17453d38b41b4a785dcc788134d1c10
VonRiddarn <vonriddarn@gmail.com>
2025-05-09 05:40:07 +0200

    feat: update AppPage layout with responsive grid and add PopularGamesList component

--- src/app/AppPage.tsx
+++ src/app/AppPage.tsx
@@ -1,6 +1,7 @@
 "use client";
 import styles from "./AppPage.module.scss";
 import Panel from "@/components/Panel/Panel";
+import PopularGamesList from "@/components/PopularGamesList/PopularGamesList";
 import { getCachedEntitiesAsArray } from "@/services/igdb/visitedEntitiesCache";
 
 const AppPage = () => {
@@ -9,6 +10,9 @@ const AppPage = () => {
 	return (
 		<main>
 			<div className={styles["content"]}>
+				<Panel className={styles["panel-popular"]} title={"Popular"}>
+					<PopularGamesList />
+				</Panel>
 				<Panel className={styles["panel-visited"]} title={"Visited"}>
 					{entities.map((er) => {
 						return (
