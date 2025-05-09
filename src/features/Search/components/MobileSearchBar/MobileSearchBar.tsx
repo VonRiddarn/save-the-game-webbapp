568c23f8de3f57763cfcff1bdfc2e6e117c34158
VonRiddarn <vonriddarn@gmail.com>
2025-05-09 04:06:44 +0200

    feat: enhance MobileSearchbar functionality by focusing input on button click and conditionally displaying dropdown

--- src/features/Search/components/MobileSearchBar/MobileSearchBar.tsx
+++ src/features/Search/components/MobileSearchBar/MobileSearchBar.tsx
@@ -2,6 +2,7 @@ import { IGDBNamedEntityReference } from "@/services/igdb/types";
 import styles from "./MobileSearchBar.module.scss";
 import SearchbarDropdown from "../SearchbarDropdown/SearchbarDropdown";
 import { useSearch } from "../../widgets/Searchbar/context/SearchContext";
+import { useRef } from "react";
 
 type MobileSearchbarProps = {
 	handleChange: (value: string) => void;
@@ -11,6 +12,8 @@ type MobileSearchbarProps = {
 const MobileSearchbar = ({ entities, handleChange }: MobileSearchbarProps) => {
 	const { currentInput, setCurrentInput, dropdownActive, setDropdownActive } = useSearch();
 
+	const inputRef = useRef<HTMLInputElement | null>(null);
+
 	return (
 		<div
 			className={`${styles["mobile-searchbar"]} ${
@@ -19,6 +22,7 @@ const MobileSearchbar = ({ entities, handleChange }: MobileSearchbarProps) => {
 		>
 			<form className={`${styles["mobile-searchbar__form"]}`}>
 				<input
+					ref={inputRef}
 					type="search"
 					value={currentInput}
 					onChange={(event) => {
@@ -28,10 +32,18 @@ const MobileSearchbar = ({ entities, handleChange }: MobileSearchbarProps) => {
 					onSubmit={(e) => e.preventDefault()}
 				/>
 			</form>
-			<SearchbarDropdown className={styles["mobile-searchbar__dropdown"]} entities={entities} />
+			<SearchbarDropdown
+				className={`${styles["mobile-searchbar__dropdown"]} ${
+					entities.length <= 0 ? styles["mobile-searchbar__dropdown--empty"] : ""
+				}`}
+				entities={entities}
+			/>
 			<button
 				className={`${styles["mobile-searchbar__button"]}`}
-				onClick={() => setDropdownActive(true)}
+				onClick={() => {
+					setDropdownActive(true);
+					inputRef.current?.focus();
+				}}
 			>
 				Click to search
 			</button>
