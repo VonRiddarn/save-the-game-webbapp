90b2821e2a422b9fa4e61b70e7761e529771f3fa
VonRiddarn <vonriddarn@gmail.com>
2025-05-09 04:50:28 +0200

    refactor: remove unused types from Search feature

--- src/features/Search/shared/types.ts
+++
@@ -1,9 +0,0 @@
-import { Game, Company, Character, IGDBNamedEntityReference } from "@/services/igdb/types";
-
-export type EntityCollection = { games: Game[]; companies: Company[]; characters: Character[] };
-
-export type SearchBarVesselProps = {
-	currentInput: string;
-	onChange: (value: string) => void;
-	entities: IGDBNamedEntityReference[];
-};
