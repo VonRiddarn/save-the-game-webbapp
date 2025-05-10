fdebd0e11c01d1c9ba74a3f3e1ecc83af3242d92
VonRiddarn <vonriddarn@gmail.com>
2025-05-10 02:09:19 +0200

    feat: integrate useSearch hook to manage dropdown state in EntityDropdownCard

--- src/features/Search/components/EntityDropdownCard/EntityDropdownCard.tsx
+++ src/features/Search/components/EntityDropdownCard/EntityDropdownCard.tsx
@@ -2,6 +2,7 @@ import styles from "./EntityDropdownCard.module.scss";
 import { useEntityAvatar } from "@/hooks/useEntityImage";
 import { IGDBMainEntityReferenceFull } from "@/services/igdb/types";
 import Link from "next/link";
+import { useSearch } from "../../widgets/Searchbar/context/SearchContext";
 
 type EntityDropdownCardProps = {
 	entityRef: IGDBMainEntityReferenceFull;
@@ -11,8 +12,16 @@ const EntityDropdownCard = ({ entityRef }: EntityDropdownCardProps) => {
 	const { entity, endpoint } = entityRef;
 	const { imageUrl } = useEntityAvatar(entityRef, "small");
 
+	const { setDropdownActive } = useSearch();
+
 	return (
-		<Link className={styles["entity-dropdown-card"]} href={`/${endpoint}/${entity.slug}`}>
+		<Link
+			className={styles["entity-dropdown-card"]}
+			href={`/${endpoint}/${entity.slug}`}
+			onClick={() => {
+				setDropdownActive(false);
+			}}
+		>
 			{/*eslint-disable-next-line @next/next/no-img-element*/}
 			<img src={imageUrl} alt={`Image of ${entity.name}`} />
 			<p>{entity.name}</p>
